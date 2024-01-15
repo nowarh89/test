1c526627f98af4c5eb5c151f04c9239d44fc1e1a
Admin <Admin@DESKTOP-5NKFECA>
2024-01-15 10:21:24 +0200

    finished ex1

---
+++ exersice-1/src/main/java/exersice_1/exersice_1/App.java
@@ -0,0 +1,13 @@
+package exersice_1.exersice_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
