a50c9f9987b556fd57a03ec1a511f516b93daabd
Admin <Admin@DESKTOP-5NKFECA>
2024-01-23 20:23:56 +0200

    finished

--- exersice-1/src/test/java/exersice_1/exersice_1/AppTest.java
+++ exersice-1/src/test/java/exersice_1/exersice_1/AppTest.java
@@ -10,11 +10,43 @@ import org.junit.Test;
 public class AppTest 
 {
     /**
-     * Rigorous Test :--)
+     * Rigorous Testt :--)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void exe_2_Partition_D_min() {
+    	String result =App.NumberToChar(60);
+    	String expected_result="D";
+    	org.junit.Assert.assertEquals(expected_result, result);
+    	
     }
+    @Test
+    public void exe_2_Partition_D_min_plus1() {
+    	String result =App.NumberToChar(61);
+    	String expected_result="D";
+    	org.junit.Assert.assertEquals(expected_result, result);
+    	
+    }
+    @Test
+    public void exe_2_Partition_D_Nominal() {
+    	String result =App.NumberToChar(65);
+    	String expected_result="D";
+    	org.junit.Assert.assertEquals(expected_result, result);
+    	
+    }
+    @Test
+    public void exe_2_Partition_D_Max_Minus1() {
+    	String result =App.NumberToChar(68);
+    	String expected_result="D";
+    	org.junit.Assert.assertEquals(expected_result, result);
+    	
+    }
+    @Test
+    public void exe_2_Partition_D_Max() {
+    	String result =App.NumberToChar(69);
+    	String expected_result="D";
+    	org.junit.Assert.assertEquals(expected_result, result);
+    	
+    }
+    
+
 }
