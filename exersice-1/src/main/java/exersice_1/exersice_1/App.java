6b27a50a0a202218a9dd29f714b1f4b20692e6a7
Admin <Admin@DESKTOP-5NKFECA>
2024-01-23 19:25:44 +0200

    finishedd

--- exersice-1/src/main/java/exersice_1/exersice_1/App.java
+++ exersice-1/src/main/java/exersice_1/exersice_1/App.java
@@ -1,13 +1,24 @@
 package exersice_1.exersice_1;
 
 /**
- * Hello world!
+ * Hello world
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!!" );
+public class App {
+    public static String NumberToChar(int number) {
+        if (1 <=number && number<= 49)
+            return "F";
+        else if (50 <=number && number<= 59)
+            return "E";
+        else if (60 <=number && number<= 69)
+            return "D";
+        else if (70 <=number && number<= 79)
+            return "C";
+        else if (80 <=number && number<= 89)
+            return "B";
+        else if (90 <=number && number<= 100)
+            return "A";
+        else
+            return "error";
     }
 }
